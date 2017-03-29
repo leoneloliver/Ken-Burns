183ed1fc0108341163bf816b2c558d01eb9c42c7
Leonel Oliveira <leoneloliver@gmail.com>
2017-03-29 09:46:09 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const kbEl = document.getElementById("kb");
+startKb();
 function startKb(){
   const kbspeed = kbEl.dataset.speed;
   const kbtype = kbEl.dataset.animation;
   document.documentElement.style.setProperty(`--kb-speed`, kbspeed);
   document.documentElement.style.setProperty(`--kb-type`, kbtype);
 }
-startKb();
+
